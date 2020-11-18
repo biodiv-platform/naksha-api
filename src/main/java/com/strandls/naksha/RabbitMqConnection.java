e84fd404ead5c28e3d7fd96604ca2e51a7c7bb64
vilay <kandi.vilay09@gmail.com>
2020-11-18 19:55:57 +0530

    fix : sonar fix

--- src/main/java/com/strandls/naksha/RabbitMqConnection.java
+++ src/main/java/com/strandls/naksha/RabbitMqConnection.java
@@ -9,17 +9,17 @@ import com.rabbitmq.client.ConnectionFactory;
 
 public class RabbitMqConnection {
 
-	private  static final String QUEUE_ELASTIC = "elastic";
+	private static final String QUEUE_ELASTIC = "elastic";
 	private static final String ROUTING_ELASTIC = "esmodule";
-	
+
 	public static final String EXCHANGE_BIODIV;
 	public static final String MAIL_QUEUE;
 	public static final String MAIL_ROUTING_KEY;
-	
+
 	static {
 		EXCHANGE_BIODIV = NakshaConfig.getString("rabbitmq_exchange");
 		MAIL_QUEUE = NakshaConfig.getString("rabbitmq_queue");
-		MAIL_ROUTING_KEY = NakshaConfig.getString("rabbitmq_routingKey");		
+		MAIL_ROUTING_KEY = NakshaConfig.getString("rabbitmq_routingKey");
 	}
 
 	public Channel setRabbitMQConnetion() throws IOException, TimeoutException {
@@ -33,13 +33,15 @@ public class RabbitMqConnection {
 		factory.setPort(rabbitmqPort);
 		factory.setUsername(rabbitmqUsername);
 		factory.setPassword(rabbitmqPassword);
-		Connection connection = factory.newConnection();
-		Channel channel = connection.createChannel();
-		channel.exchangeDeclare(EXCHANGE_BIODIV, "direct");
-		channel.queueDeclare(QUEUE_ELASTIC, false, false, false, null);
-		channel.queueBind(QUEUE_ELASTIC, EXCHANGE_BIODIV, ROUTING_ELASTIC);
-		channel.queueDeclare(MAIL_QUEUE, false, false, false, null);
-		channel.queueBind(MAIL_QUEUE, EXCHANGE_BIODIV, MAIL_ROUTING_KEY);
-		return channel;
+		try (Connection connection = factory.newConnection()) {
+			try (Channel channel = connection.createChannel()) {
+				channel.exchangeDeclare(EXCHANGE_BIODIV, "direct");
+				channel.queueDeclare(QUEUE_ELASTIC, false, false, false, null);
+				channel.queueBind(QUEUE_ELASTIC, EXCHANGE_BIODIV, ROUTING_ELASTIC);
+				channel.queueDeclare(MAIL_QUEUE, false, false, false, null);
+				channel.queueBind(MAIL_QUEUE, EXCHANGE_BIODIV, MAIL_ROUTING_KEY);
+				return channel;
+			}
+		}
 	}
 }
