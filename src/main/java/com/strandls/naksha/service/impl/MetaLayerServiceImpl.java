beba46b4b71f68a33795405afd62858237ff4e0f
Guddu Kumar Sharma <guddusharma027@gmail.com>
2022-03-25 09:28:02 +0530

    dummy1

--- src/main/java/com/strandls/naksha/service/impl/MetaLayerServiceImpl.java
+++ src/main/java/com/strandls/naksha/service/impl/MetaLayerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.strandls.naksha.service.impl;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -434,8 +436,12 @@ public class MetaLayerServiceImpl extends AbstractService<MetaLayer> implements 
 			throw new IOException("Shape file creation failed");
 		} else {
 			System.out.println("======================org2org started====================== START");
-			process.waitFor(20000, TimeUnit.MILLISECONDS);
-			process.destroy();
+			// process.waitFor(20000, TimeUnit.MILLISECONDS);
+			// process.getErrorStream();
+			// process.getOutputStream();
+			printErrorStream(process);
+			printInputStream(process);
+			process.waitFor();
 			System.out.println(process.getOutputStream());
 			System.out.println(process.getErrorStream());
 			System.out.println("process.waitFor()..");
@@ -470,6 +476,24 @@ public class MetaLayerServiceImpl extends AbstractService<MetaLayer> implements 
 		// return directory.getAbsolutePath();
 	}
 
+	public static void printInputStream(Process process ) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		String line = "";
+		
+		while((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
+	}
+	
+	public static void printErrorStream(Process process ) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		String line = "";
+		
+		while((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
+	}
+	
 	public void zipFolder(String zipFileLocation, File fileDirectory) throws IOException {
 		FileOutputStream fos = new FileOutputStream(zipFileLocation);
 		try (ZipOutputStream zipOut = new ZipOutputStream(fos)) {
