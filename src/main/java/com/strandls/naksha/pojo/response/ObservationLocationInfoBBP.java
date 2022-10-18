9452fb45845625b61cabfa34cedbaed0ca5128e1
prakhar-s <prakhardevsingh4@gmail.com>
2022-10-18 14:19:47 +0530

    layer integration for bbp

---
+++ src/main/java/com/strandls/naksha/pojo/response/ObservationLocationInfoBBP.java
@@ -0,0 +1,34 @@
+package com.strandls.naksha.pojo.response;
+
+public class ObservationLocationInfoBBP extends ObservationLocationInfo {
+	private String dzongkhag;
+	private String geog;
+
+	public ObservationLocationInfoBBP(String soil, String temp, String rainfall, String tahsil, String forestType) {
+		super(soil, temp, rainfall, tahsil, forestType);
+	}
+
+	public ObservationLocationInfoBBP(String soil, String temp, String rainfall, String tahsil, String forestType,
+			String dzongkhag, String geog) {
+		super(soil, temp, rainfall, tahsil, forestType);
+		this.dzongkhag = dzongkhag;
+		this.geog = geog;
+	}
+
+	public String getDzongkhag() {
+		return dzongkhag;
+	}
+
+	public void setDzongkhag(String dzongkhag) {
+		this.dzongkhag = dzongkhag;
+	}
+
+	public String getGeog() {
+		return geog;
+	}
+
+	public void setGeog(String geog) {
+		this.geog = geog;
+	}
+
+}
